56a2bc9a8ff3e422a853ca0d281aa0768ff5bfc0
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-01 10:20:18 -0400

    Initial commit

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../messages.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html'
+})
+export class MessagesComponent {
+  constructor(private messageService: MessageService) {
+
+  }
+}
